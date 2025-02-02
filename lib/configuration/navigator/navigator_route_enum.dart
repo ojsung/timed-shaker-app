1eddc87a33dfe84e95ace4a397208534e7cec64a
ojsung <jacob.sung@lendio.com>
2025-02-02 11:20:46 -0700

    Reset app for flutter upgrade - Restart app from beginning - Remove all existing libs and code - Maintain git history

--- lib/configuration/navigator/navigator_route_enum.dart
+++
@@ -1,9 +0,0 @@
-enum NavigatorRoute {
-  home,
-  newRoutine;
-
-  static NavigatorRoute matchRoute(String routeName) {
-    return NavigatorRoute.values.firstWhere((element) => element.name == routeName,
-        orElse: () => throw Exception('Path $routeName does not exist'));
-  }
-}
